5578dc9233e0c5b0d15835a506446b3ba19b46bd
isaakwheeler <77463529+isaakwheeler@users.noreply.github.com>
2021-12-05 14:21:34 -0800

    save point

---
+++ app/src/main/java/edu/cascadia/mobas/campusguidebook/EventItems.java
@@ -0,0 +1,21 @@
+package edu.cascadia.mobas.campusguidebook;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+class EventItems extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_event_items, container, false);
+    }
+}
