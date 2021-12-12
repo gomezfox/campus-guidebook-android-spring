25533d7ee594781e535b3e90846bd4f9b432af3b
SteveSteveSteveSteveSteveSteve <76667786+SteveSteveSteveSteveSteveSteve@users.noreply.github.com>
2021-12-12 15:14:05 -0800

    navigation added to sustainability info

--- app/src/main/java/edu/cascadia/mobas/campusguidebook/ui/info/InfoFragment.java
+++ app/src/main/java/edu/cascadia/mobas/campusguidebook/ui/info/InfoFragment.java
@@ -27,7 +27,8 @@ public class InfoFragment extends Fragment {
         binding = FragmentInfoBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textInfo;
+        final TextView textView = binding.textView12;
+        // changed this to textView2
         galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
