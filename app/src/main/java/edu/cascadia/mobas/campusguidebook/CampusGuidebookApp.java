35cb4f83f149c350f4d9f19bf2abea78b6f228e4
gomezfox <stephen@gomezfox.net>
2022-02-01 06:26:10 -0800

    Extends Application to inject dependencies into database/repository

---
+++ app/src/main/java/edu/cascadia/mobas/campusguidebook/CampusGuidebookApp.java
@@ -0,0 +1,36 @@
+package edu.cascadia.mobas.campusguidebook;
+
+import android.app.Application;
+
+import edu.cascadia.mobas.campusguidebook.data.database.AppDatabase;
+import edu.cascadia.mobas.campusguidebook.data.repository.AppRepository;
+
+// Top level class to implement inject dependencies
+// and make database access through viewmodels/repositories
+// available everywhere it is needed in the app
+
+public class CampusGuidebookApp extends Application {
+    private AppExecutors appExecutors;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        appExecutors = new AppExecutors();
+    }
+
+    // return an instance of the database
+    public AppDatabase getAppDatabase() {
+        return AppDatabase.getInstance(this, appExecutors);
+    }
+
+    // return an instance of the repository
+    public AppRepository getAppRepository() {
+        return AppRepository.getInstance(getAppDatabase(), appExecutors);
+    }
+
+    // make this executor pool available
+    public AppExecutors getAppExecutors() {
+        return appExecutors;
+    }
+}
+
