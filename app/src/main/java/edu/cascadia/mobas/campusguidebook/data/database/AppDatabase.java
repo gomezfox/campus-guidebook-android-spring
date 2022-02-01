ea881f5256727cec1255663c16402baf227506af
gomezfox <stephen@gomezfox.net>
2022-02-01 07:58:56 -0800

    AppDatabase utilizes Executor pool and transaction to populate data

--- app/src/main/java/edu/cascadia/mobas/campusguidebook/data/database/AppDatabase.java
+++ app/src/main/java/edu/cascadia/mobas/campusguidebook/data/database/AppDatabase.java
@@ -1,21 +1,19 @@
 package edu.cascadia.mobas.campusguidebook.data.database;
 
-import android.content.Context;
-import android.os.AsyncTask;
+import android.app.Application;
 import androidx.annotation.NonNull;
 import androidx.room.Database;
+import androidx.room.TypeConverters;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
-import androidx.room.TypeConverters;
 import androidx.sqlite.db.SupportSQLiteDatabase;
 
-import edu.cascadia.mobas.campusguidebook.data.dao.ClubDao;
-import edu.cascadia.mobas.campusguidebook.data.dao.EventDao;
-import edu.cascadia.mobas.campusguidebook.data.dao.SustainabilityDao;
-import edu.cascadia.mobas.campusguidebook.data.dao.UserDao;
+import edu.cascadia.mobas.campusguidebook.data.SampleData;
+import edu.cascadia.mobas.campusguidebook.data.dao.*;
 import edu.cascadia.mobas.campusguidebook.data.model.*;
+import edu.cascadia.mobas.campusguidebook.data.typeconverter.*;
+import edu.cascadia.mobas.campusguidebook.AppExecutors;
 
-import edu.cascadia.mobas.campusguidebook.data.typeconverter.OffsetDateTimeConverter;
 
 // adding annotation for our database entities and db version.
 @Database(entities = {
@@ -27,8 +25,12 @@ import edu.cascadia.mobas.campusguidebook.data.typeconverter.OffsetDateTimeConve
 public abstract class AppDatabase extends RoomDatabase {
 
     // below line is to create instance
-    // for our database class.
+    // for our singleton database class.
     private static AppDatabase instance;
+
+    // below line is to save the executor pool
+    // to create database and supply repositories
+    private AppExecutors appExecutors;
 
     // below line is to create
     // abstract variable for dao.
@@ -38,52 +40,51 @@ public abstract class AppDatabase extends RoomDatabase {
     public abstract UserDao UserDao();
 
     // on below line we are getting instance for our database.
-    public static synchronized AppDatabase getInstance(Context context) {
-        // below line is to check if
-        // the instance is null or not.
-        if (instance == null) {
+    public AppDatabase getInstance(Application application, AppExecutors appExecutors) {
+        // return existing singleton instance
+        // unless the instance is null
+        if (instance != null) { return instance; }
+        // otherwise block access until database is built
+        synchronized (AppDatabase.class) {
             // if the instance is null we
             // are creating a new instance
+            this.appExecutors = appExecutors;
             instance =
-                    // for creating a instance for our database
-                    // we are creating a database builder and passing
-                    // our database class with our database name.
-                    Room.databaseBuilder(context.getApplicationContext(),
-                            AppDatabase.class, "appDatabase")
-                            // below line is use to add fall back to
-                            // destructive migration to our database.
-                            .fallbackToDestructiveMigration()
-                            // below line is to add callback
-                            // to our database.
-                            .addCallback(roomCallback)
-                            // below line is to
-                            // build our database.
-                            .build();
-        }
-        // after creating an instance
-        // we are returning our instance
-        return instance;
-    }
-
-    // below line is to create a callback for our room database.
-    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback() {
-        @Override
-        public void onCreate(@NonNull SupportSQLiteDatabase db) {
-            super.onCreate(db);
-            // this method is called when database is created
-            // and below line is to populate our data.
-            new PopulateDbAsyncTask(instance).execute();
-        }
-    };
-
-    // we are creating an async task class to perform task in background.
-    private static class PopulateDbAsyncTask extends AsyncTask<Void, Void, Void> {
-        PopulateDbAsyncTask(AppDatabase instance) {
-            EventDao dao = instance.EventDao();
-        }
-        @Override
-        protected Void doInBackground(Void... voids) {
-            return null;
-        }
-    }
+                // for creating a instance for our database
+                // we are creating a database builder and passing
+                // our database class with our database name.
+                Room.databaseBuilder(application.getApplicationContext(),
+                    AppDatabase.class, "appDatabase")
+                // below line is use to add fall back to
+                // destructive migration to our database.
+                    .fallbackToDestructiveMigration()
+                    // below line add callback that the builder executes
+                    // after build is complete and instance exists
+                    .addCallback(new RoomDatabase.Callback() {
+                        @Override
+                        public void onCreate(SupportSQLiteDatabase sqLiteDatabase) {
+                            super.onCreate(sqLiteDatabase);
+                            // this method is called after
+                            // the database build
+                            appExecutors.diskIO().execute(() -> {
+                                // use a transaction to insert
+                                // all the sample data in one pass
+                                getInstance(application, appExecutors).runInTransaction(() -> {
+                                    instance.EventDao().insert(SampleData.events());
+                                    instance.ClubDao().insert(SampleData.clubs());
+                                    instance.SustainabilityDao().insert(SampleData.sustainability());
+                                    instance.UserDao().insert(SampleData.users());
+                                });
+                            });
+                        }
+                    })
+                   .build();  // creates database then executes callback above
+            // return the newly created singleton instance
+            // note: callback may still be populating database
+            return instance;
+        } // End of synchronized block
+    } // End of getInstance
 }
+
+
+
