8ee95f73c6a2a66c41dd1bc91bfca8488d651088
gomezfox <stephen@gomezfox.net>
2022-02-03 16:02:31 -0800

    Implements lambdas for readability

--- app/src/main/java/edu/cascadia/mobas/campusguidebook/data/repository/AppRepository.java
+++ app/src/main/java/edu/cascadia/mobas/campusguidebook/data/repository/AppRepository.java
@@ -2,31 +2,32 @@ package edu.cascadia.mobas.campusguidebook.data.repository;
 
 import android.app.Application;
 import android.os.AsyncTask;
+import android.os.Build;
+import androidx.annotation.RequiresApi;
 import androidx.lifecycle.LiveData;
 import java.util.List;
-
 import edu.cascadia.mobas.campusguidebook.AppExecutors;
 import edu.cascadia.mobas.campusguidebook.data.database.AppDatabase;
 import edu.cascadia.mobas.campusguidebook.data.dao.*;
 import edu.cascadia.mobas.campusguidebook.data.model.*;
 
-
+@RequiresApi(api = Build.VERSION_CODES.O)
 public class AppRepository {
 
     // instance variable for singleton
     private static AppRepository sAppRepository = null;
-
-    private static AppDatabase mAppDatabase = null;
-    private static AppExecutors mAppExecutors = null;
-
+    private AppDatabase mAppDatabase = null;
+    private AppExecutors mAppExecutors = null;
 
     // private constructor to enforce singleton pattern
     private AppRepository() {}
 
     private AppRepository(AppDatabase db, AppExecutors executors) {
-        if (sAppRepository != null) return;
-        mAppDatabase = db;
-        mAppExecutors = executors;
+        if (sAppRepository == null) {
+            mAppDatabase = db;
+            mAppExecutors = executors;
+            sAppRepository = this;
+        }
     }
 
     // returns the AppRepository instance, creating if necessary
@@ -58,127 +59,103 @@ public class AppRepository {
         return mAppDatabase.SustainabilityDao().getAllSustainability();
     }
 
-
     // Additional Event CRUD operations
-    public static void insert(Event... events) {
-        mAppExecutors.diskIO().execute( () -> {
-            mAppDatabase.runInTransaction( () -> {
-                for (Event event:events) {
-                    mAppDatabase.EventDao().insert(event);
-                }
-            });
-        });
-    }
-    public static void update(Event... events) {
-        mAppExecutors.diskIO().execute( () -> {
-            mAppDatabase.runInTransaction( () -> {
-                for (Event event:events) {
-                    mAppDatabase.EventDao().update(event);
-                }
-            });
-        });
+    public void insert(Event... events) {
+        mAppExecutors.diskIO().execute( () -> mAppDatabase.runInTransaction( () -> {
+            for (Event event:events) {
+                mAppDatabase.EventDao().insert(event);
+            }
+        }));
     }
 
-    public static void delete(Event... events) {
-        mAppExecutors.diskIO().execute( () -> {
-            mAppDatabase.runInTransaction( () -> {
-                for (Event event : events) {
-                    mAppDatabase.EventDao().delete(event);
-                }
-            });
-        });
+    public void update(Event... events) {
+        mAppExecutors.diskIO().execute( () -> mAppDatabase.runInTransaction( () -> {
+            for (Event event:events) {
+                mAppDatabase.EventDao().update(event);
+            }
+        }));
+    }
+
+    public void delete(Event... events) {
+        mAppExecutors.diskIO().execute( () -> mAppDatabase.runInTransaction( () -> {
+            for (Event event : events) {
+                mAppDatabase.EventDao().delete(event);
+            }
+        }));
     }
 
     // Additional Club CRUD operations
-    public static void insert(Club... clubs) {
-        mAppExecutors.diskIO().execute( () -> {
-            mAppDatabase.runInTransaction( () -> {
-                for (Club club : clubs) {
-                    mAppDatabase.ClubDao().insert(club);
-                }
-            });
-        });
+    public void insert(Club... clubs) {
+        mAppExecutors.diskIO().execute( () -> mAppDatabase.runInTransaction( () -> {
+            for (Club club : clubs) {
+                mAppDatabase.ClubDao().insert(club);
+            }
+        }));
     }
 
-    public static void update(Club... clubs) {
-        mAppExecutors.diskIO().execute( () -> {
-            mAppDatabase.runInTransaction( () -> {
-                for (Club club : clubs) {
-                    mAppDatabase.ClubDao().update(club);
-                }
-            });
-        });
+    public void update(Club... clubs) {
+        mAppExecutors.diskIO().execute( () -> mAppDatabase.runInTransaction( () -> {
+            for (Club club : clubs) {
+                mAppDatabase.ClubDao().update(club);
+            }
+        }));
     }
 
-    public static void delete(Club... clubs) {
-        mAppExecutors.diskIO().execute( () -> {
-            mAppDatabase.runInTransaction( () -> {
-                for (Club club : clubs) {
-                    mAppDatabase.ClubDao().delete(club);
-                }
-            });
-        });
+    public void delete(Club... clubs) {
+        mAppExecutors.diskIO().execute( () -> mAppDatabase.runInTransaction( () -> {
+            for (Club club : clubs) {
+                mAppDatabase.ClubDao().delete(club);
+            }
+        }));
     }
 
     // Additional Sustainability CRUD operations
-    public static void insert(Sustainability... sustainabilities) {
-        mAppExecutors.diskIO().execute( () -> {
-            mAppDatabase.runInTransaction( () -> {
-                for (Sustainability sustainability : sustainabilities) {
-                    mAppDatabase.SustainabilityDao().insert(sustainability);
-                }
-            });
-        });
+    public void insert(Sustainability... sustainabilities) {
+        mAppExecutors.diskIO().execute( () -> mAppDatabase.runInTransaction( () -> {
+            for (Sustainability sustainability : sustainabilities) {
+                mAppDatabase.SustainabilityDao().insert(sustainability);
+            }
+        }));
     }
 
-    public static void update(Sustainability... sustainabilities) {
-        mAppExecutors.diskIO().execute( () -> {
-            mAppDatabase.runInTransaction( () -> {
-                for (Sustainability sustainability : sustainabilities) {
-                    mAppDatabase.SustainabilityDao().update(sustainability);
-                }
-            });
-        });
+    public void update(Sustainability... sustainabilities) {
+        mAppExecutors.diskIO().execute( () -> mAppDatabase.runInTransaction( () -> {
+            for (Sustainability sustainability : sustainabilities) {
+                mAppDatabase.SustainabilityDao().update(sustainability);
+            }
+        }));
     }
 
-    public static void delete(Sustainability... sustainabilities) {
-        mAppExecutors.diskIO().execute( () -> {
-            mAppDatabase.runInTransaction( () -> {
-                for (Sustainability sustainability : sustainabilities) {
-                    mAppDatabase.SustainabilityDao().delete(sustainability);
-                }
-            });
-        });
+    public void delete(Sustainability... sustainabilities) {
+        mAppExecutors.diskIO().execute( () -> mAppDatabase.runInTransaction( () -> {
+            for (Sustainability sustainability : sustainabilities) {
+                mAppDatabase.SustainabilityDao().delete(sustainability);
+            }
+        }));
     }
 
     // Additional User CRUD operations
-    public static void insert(User... users) {
-        mAppExecutors.diskIO().execute( () -> {
-            mAppDatabase.runInTransaction( () -> {
-                for (User user : users) {
-                    mAppDatabase.UserDao().insert(user);
-                }
-            });
-        });
+    public void insert(User... users) {
+        mAppExecutors.diskIO().execute( () -> mAppDatabase.runInTransaction( () -> {
+            for (User user : users) {
+                mAppDatabase.UserDao().insert(user);
+            }
+        }));
     }
 
-    public static void update(User... users) {
-        mAppExecutors.diskIO().execute( () -> {
-            mAppDatabase.runInTransaction( () -> {
-                for (User user : users) {
-                    mAppDatabase.UserDao().update(user);
-                }
-            });
-        });
+    public void update(User... users) {
+        mAppExecutors.diskIO().execute( () -> mAppDatabase.runInTransaction( () -> {
+            for (User user : users) {
+                mAppDatabase.UserDao().update(user);
+            }
+        }));
     }
 
-    public static void delete(User... users) {
-        mAppExecutors.diskIO().execute( () -> {
-            mAppDatabase.runInTransaction( () -> {
-                for (User user : users) {
-                    mAppDatabase.UserDao().delete(user);
-                }
-            });
-        });
+    public void delete(User... users) {
+        mAppExecutors.diskIO().execute( () -> mAppDatabase.runInTransaction( () -> {
+            for (User user : users) {
+                mAppDatabase.UserDao().delete(user);
+            }
+        }));
     }
 }
