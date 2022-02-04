063b448e5d612943f56a72193923910642598aa5
gomezfox <stephen@gomezfox.net>
2022-02-03 16:09:38 -0800

    Adds a field to sustainability entity for description

--- app/src/main/java/edu/cascadia/mobas/campusguidebook/data/model/Sustainability.java
+++ app/src/main/java/edu/cascadia/mobas/campusguidebook/data/model/Sustainability.java
@@ -5,37 +5,48 @@ import androidx.room.PrimaryKey;
 
 @Entity(tableName = "Sustainability_Table")
 public class Sustainability {
+
     // below line is to auto increment
     // id for each Sustainability.
     @PrimaryKey(autoGenerate = true)
 
-    // variable for our id.
+    // entity field for our id.
     private int id;
 
-    // below line is a variable
-    // for Event name.
+    // entity field for sustainability name
     @ColumnInfo(name = "sustainability_name")
     private String sustainabilityName;
 
+    // entity field for sustainability description
+    @ColumnInfo(name = "sustainability_description")
+    private String sustainabilityDescription;
 
     // below line we are creating constructor class.
     // inside constructor class we are not passing
     // our id because it is incrementing automatically
-
     public Sustainability(String sustainabilityName) {
         this.sustainabilityName = sustainabilityName;
     }
 
-    // on below line we are creating
+    // on below lines we are creating
     // getter and setter methods.
     public String getSustainabilityName() {
         return sustainabilityName;
     }
     public void setSustainabilityName(String name) { this.sustainabilityName = name; }
 
+    public String getSustainabilityDescription() {
+        return sustainabilityDescription;
+    }
+
+    public void setSustainabilityDescription(String sustainabilityDescription) {
+        this.sustainabilityDescription = sustainabilityDescription;
+    }
+
     public int getId() {
         return id;
     }
+
     public void setId(int ID) {
         this.id = ID;
     }
