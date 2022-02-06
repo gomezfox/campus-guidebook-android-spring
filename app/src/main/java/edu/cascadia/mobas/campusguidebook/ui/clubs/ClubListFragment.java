9b6d487823222bdc252e4ef3d0df22c3e3d97ea5
gomezfox <stephen@gomezfox.net>
2022-02-05 21:54:37 -0800

    Fixes fragment xml

---
+++ app/src/main/java/edu/cascadia/mobas/campusguidebook/ui/clubs/ClubListFragment.java
@@ -0,0 +1,36 @@
+package edu.cascadia.mobas.campusguidebook.ui.clubs;
+
+import androidx.lifecycle.ViewModelProvider;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.cascadia.mobas.campusguidebook.R;
+import edu.cascadia.mobas.campusguidebook.viewmodel.ClubListViewModel;
+
+public class ClubListFragment extends Fragment {
+
+    private ClubListViewModel mViewModel;
+
+    public static ClubListFragment newInstance() {
+        return new ClubListFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_club_list, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(ClubListViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
