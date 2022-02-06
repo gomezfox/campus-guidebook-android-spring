4b06bd08082b8cdec8da6c957ab7d3b6dda34ff3
gomezfox <stephen@gomezfox.net>
2022-02-05 22:28:18 -0800

    Adds nav_club_list to AppBarConfiguration

--- app/src/main/java/edu/cascadia/mobas/campusguidebook/MainActivity.java
+++ app/src/main/java/edu/cascadia/mobas/campusguidebook/MainActivity.java
@@ -36,7 +36,10 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_info, R.id.nav_events)
+                R.id.nav_home,
+                R.id.nav_info,
+                R.id.nav_events,
+                R.id.nav_club_list)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
