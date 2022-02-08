cd7adbe15fdbacbebd8068b87d7854a8d5657124
gomezfox <stephen@gomezfox.net>
2022-02-07 17:48:49 -0800

    ClubListRecycler fragment and adapter complete

--- app/src/main/java/edu/cascadia/mobas/campusguidebook/viewmodel/ClubListViewModel.java
+++ app/src/main/java/edu/cascadia/mobas/campusguidebook/viewmodel/ClubListViewModel.java
@@ -7,6 +7,10 @@ import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.List;
 import edu.cascadia.mobas.campusguidebook.CampusGuidebookApp;
 import edu.cascadia.mobas.campusguidebook.data.model.Club;
@@ -16,18 +20,23 @@ import edu.cascadia.mobas.campusguidebook.data.repository.AppRepository;
 public class ClubListViewModel extends AndroidViewModel {
 
     private AppRepository mAppRepository = null;
-    private LiveData<List<Club>> mAllClubs;
+    private MutableLiveData<ArrayList<Club>>mClubListLiveData
+            = new MutableLiveData<ArrayList<Club>>();
 
     public ClubListViewModel(@NonNull Application application) {
         super(application);
         // gets a reference to the repository
         mAppRepository = ((CampusGuidebookApp) application).getAppRepository();
-        // obtains the LiveData list of all clubs provided from the repository
-        mAllClubs = mAppRepository.getAllClubs();
+        // and use it to set the value of the LiveData
+        mClubListLiveData.setValue((ArrayList<Club>)mAppRepository.getAllClubs().getValue());
     }
 
-    public LiveData<List<Club>> getAllClubs() {
-        return mAllClubs;
+    public MutableLiveData<ArrayList<Club>> getAllClubs() {
+        return mClubListLiveData;
+    }
+    public MutableLiveData<ArrayList<Club>> searchPost(String query) {
+        // add a search query to the DAO and the repository
+        return new MutableLiveData<ArrayList<Club>>();
     }
 }
 
