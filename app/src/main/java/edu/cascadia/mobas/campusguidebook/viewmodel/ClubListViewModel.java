1d44db838efd51d9d1258a55dae66ea80d166103
gomezfox <stephen@gomezfox.net>
2022-02-10 15:01:03 -0800

    ViewModel uses MediatorLiveData

--- app/src/main/java/edu/cascadia/mobas/campusguidebook/viewmodel/ClubListViewModel.java
+++ app/src/main/java/edu/cascadia/mobas/campusguidebook/viewmodel/ClubListViewModel.java
@@ -2,15 +2,13 @@ package edu.cascadia.mobas.campusguidebook.viewmodel;
 
 import android.app.Application;
 import android.os.Build;
+import android.provider.MediaStore;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
-
-import java.lang.reflect.Array;
-import java.util.ArrayList;
+import androidx.lifecycle.MediatorLiveData;
 import java.util.List;
 import edu.cascadia.mobas.campusguidebook.CampusGuidebookApp;
 import edu.cascadia.mobas.campusguidebook.data.model.Club;
@@ -20,23 +18,22 @@ import edu.cascadia.mobas.campusguidebook.data.repository.AppRepository;
 public class ClubListViewModel extends AndroidViewModel {
 
     private AppRepository mAppRepository = null;
-    private MutableLiveData<ArrayList<Club>>mClubListLiveData
-            = new MutableLiveData<ArrayList<Club>>();
+    private final MediatorLiveData<List<Club>> mLiveClubList = new MediatorLiveData<List<Club>>();
 
     public ClubListViewModel(@NonNull Application application) {
         super(application);
         // gets a reference to the repository
         mAppRepository = ((CampusGuidebookApp) application).getAppRepository();
-        // and use it to set the value of the LiveData
-        mClubListLiveData.setValue((ArrayList<Club>)mAppRepository.getAllClubs().getValue());
+        // Adds repository source for textual live data
+        mLiveClubList.addSource(mAppRepository.getAllClubs(), value -> {
+            mLiveClubList.setValue(value);
+        });
+        //TODO: Add a source and observe image data
     }
 
-    public MutableLiveData<ArrayList<Club>> getAllClubs() {
-        return mClubListLiveData;
+    public LiveData<List<Club>> getAllClubs() {
+        return mLiveClubList;
     }
-    public MutableLiveData<ArrayList<Club>> searchPost(String query) {
-        // add a search query to the DAO and the repository
-        return new MutableLiveData<ArrayList<Club>>();
-    }
+
 }
 
