05a75ed6812efff26749364267d683ff8c47ca90
gomezfox <stephen@gomezfox.net>
2022-02-10 14:57:36 -0800

    Initialization work performed during splash display

--- app/src/main/java/edu/cascadia/mobas/campusguidebook/Splash.java
+++ app/src/main/java/edu/cascadia/mobas/campusguidebook/Splash.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.content.Intent;
 import android.util.Log;
+import edu.cascadia.mobas.campusguidebook.data.database.AppDatabase;
 import edu.cascadia.mobas.campusguidebook.data.repository.AppRepository;
 import edu.cascadia.mobas.campusguidebook.ui.login.LoginActivity;
 
@@ -18,19 +19,23 @@ public class Splash extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        // Initialize the database and repositories
-        AppRepository _dummy_ = ((CampusGuidebookApp) getApplication()).getAppRepository();
-        Log.d("Splash", "Repository creation " + (_dummy_==null ? "FAILED" : "SUCCESS"));
-
-        Handler handler = new Handler();
-
-        handler.postDelayed( () -> {
+        (new Handler).postDelayed( () -> {
             // prepare to navigate directly to main activity
             Intent intent = new Intent(Splash.this, MainActivity.class);
             startActivity(intent);
             finish();
-
         },1500);
+
+        // Initialize the database and repositories while splash displays
+        final AppRepository repo = ((CampusGuidebookApp) getApplication()).getAppRepository();
+        Log.d("Splash", "Repository creation " + (repo==null ? "FAILED" : "SUCCESS"));
+        final AppDatabase db = ((CampusGuidebookApp) getApplication()).getAppDatabase();
+        Log.d("Splash", "Database creation " + (repo==null ? "FAILED" : "SUCCESS"));
+        final AppExecutors executors = ((CampusGuidebookApp) getApplication()).getAppExecutors();
+        executors.diskIO().execute( () -> {
+            Integer count = db.ClubDao().getClubCount();
+            Log.d("Splash", "Club count is " + count.toString());
+        });
     }
 }
 
