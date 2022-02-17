34dc6de0ac23124343dec93be5a5ed49721beff2
gomezfox <stephen@gomezfox.net>
2022-02-17 11:16:00 -0800

    ClubListAdapter updated to show images, Observer specifies lifecycle

--- app/src/main/java/edu/cascadia/mobas/campusguidebook/ui/clubs/ClubListAdapter.java
+++ app/src/main/java/edu/cascadia/mobas/campusguidebook/ui/clubs/ClubListAdapter.java
@@ -1,7 +1,6 @@
 package edu.cascadia.mobas.campusguidebook.ui.clubs;
 
-import android.content.Context;
-import android.util.Log;
+import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,97 +8,95 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.cardview.widget.CardView;
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
-import androidx.recyclerview.widget.DiffUtil;
+import androidx.lifecycle.LifecycleOwner;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.ArrayList;
 import java.util.List;
 import edu.cascadia.mobas.campusguidebook.R;
-import edu.cascadia.mobas.campusguidebook.data.model.Club;
+import edu.cascadia.mobas.campusguidebook.viewmodel.ClubListViewModel;
 
+@RequiresApi(api = Build.VERSION_CODES.O)
 public class ClubListAdapter extends RecyclerView.Adapter<ClubListAdapter.ViewHolder> {
 
-    private List<Club> mClubList;
-    private Context mContext;
+    private List<ClubUIItem> mClubList;
+    private final ClubListViewModel mViewModel;
+    private LifecycleOwner mLifecycleOwner;
 
-    // Viewholder provides access to the individual views in a row of the RecyclerView's row
+    // ViewHolder provides access to the individual views in a row of the RecyclerView's row
+    // This internal static class is supplied in the diamond brackets in the class declaration
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
-        private final CardView clubCardView;
-        private final ImageView clubImageView;
-        private final TextView clubTextView;
+        // Provide variables to hold references to each View in the club list item fragment
+        public final CardView clubCardView;
+        public final ImageView clubImageView;
+        public final TextView clubNameTextView;
 
+        // The constructor takes a parent view (LinearLayout) on the club list item fragment
+        // and allows us to get references to the views it contains
         public ViewHolder(View parentView) {
             super(parentView);
-            // TODO: Define click listener(s) for the ViewHolder's Views
-            clubCardView = (CardView) parentView.findViewById(R.id.cardView_club_list);
-            clubImageView = (ImageView) parentView.findViewById(R.id.imageView_club_banner);
-            clubTextView = (TextView) parentView.findViewById(R.id.textView_club_title);
+            // look up and assign the views in the club list item fragment to our variables
+            clubCardView = parentView.findViewById(R.id.cardView_club_list);
+            clubImageView = parentView.findViewById(R.id.imageView_club_banner);
+            clubNameTextView = parentView.findViewById(R.id.textView_club_title);
+            // TODO: Define click listener(s) for the ViewHolder's CardView
         }
+    }  // End of static ViewHolder class
 
-        // These methods return references to the views on the current row
-        public TextView getTextView() {
-            return clubTextView;
-        }
-
-        public ImageView getImageView() {
-            return clubImageView;
-        }
-
-        public CardView getCardView() {
-            return clubCardView;
-        }
-
+    // ClubListAdapter constructor and methods
+    // Initialize this adapter with a reference to the datasource to be used.
+    public ClubListAdapter(List<ClubUIItem> clubList, ClubListViewModel viewModel) {
+        mClubList = clubList;
+        mViewModel = viewModel;
     }
 
-    // Updates the list of clubs used
-    public void setData(List<Club> newClubList) {
+    // TODO: Investigate using SwitchMap to modify instead of completely replacing the list
+    // Updates the list of clubs used by the RecyclerView
+    public void setList(List<ClubUIItem> newClubList) {
         this.mClubList = newClubList;
         notifyDataSetChanged();
     }
 
-    // Initialize the datasource used by the Adapter.
-    public ClubListAdapter(List<Club> clubList) {
-        mClubList = clubList;
-    }
-
-    // Used by the layout manager to return a ViewHolder for a row containing club info
+    // Used by the layout manager to return the ViewHolder for a row containing club info
     @Override
     @NonNull
-    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
-        // Create a new view, which defines the UI of the list item
-        View view = LayoutInflater.from(viewGroup.getContext())
-                .inflate(R.layout.fragment_club_list_item, viewGroup, false);
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parentViewGroup, int viewType) {
+
+        // get the parent LifecycleOwner if this is the first time
+        if (mLifecycleOwner == null) {
+            mLifecycleOwner = (LifecycleOwner) parentViewGroup.getContext();
+        }
+
+        // Create a new view containing all the views for the UI of the list item
+        View view = LayoutInflater.from(parentViewGroup.getContext())
+                .inflate(R.layout.fragment_club_list_item, parentViewGroup, false);
+
         return new ViewHolder(view);
     }
 
     // Used by the layout manager to change the displayed values in a bound ViewHolder
     @Override
-    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, final int position) {
+
         // Get the data for a particular row within the list; return if null
-        Club clubItem = mClubList.get(position);
-        if (clubItem == null) {
-            Log.d("ClubListAdapter", "Club item is null");
+        ClubUIItem clubUIItem = mClubList.get(position);
+
+        // return if no data
+        if (clubUIItem == null || clubUIItem.getClub() == null) {
             return;
         }
 
-        String clubName = clubItem.getClubName();
-        Log.d("ClubListAdapter", "Club name is " + clubName);
-
-
-        // display the data from clubItem in the Views provided by the ViewHolder
-        TextView tv = viewHolder.getTextView();
-        Log.d("ClubListAdapter", "getTextView returns " + (tv == null ? "null" : "view"));
-        if (tv == null) {
-            return;
+        // otherwise, fill in the ui with the clubItem data, using null checks for safety
+        if (viewHolder.clubNameTextView != null) {
+            viewHolder.clubNameTextView.setText(clubUIItem.getClub().getClubName());
         }
-        Log.d("ClubListAdapter", "Setting textview to " + clubName);
-        tv.setText(clubName);
-        // TODO: replace static image below with one based on uri in Club.getUri
-        // viewHolder.getImageView().setImageResource(R.drawable.engineer_s_mindset__1_);
+
+        // the image is livedata so it needs an observer
+        if (viewHolder.clubImageView != null) {
+            clubUIItem.getImage().observe(mLifecycleOwner, viewHolder.clubImageView::setImageDrawable);
+        }
     }
 
     // Used by the layout manager to determine the number of clubs in the list
