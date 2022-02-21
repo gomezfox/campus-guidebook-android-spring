acedd2cfc0c896660a42a552cb4c09e056ac99ea
Isaak Wheeler <isaak.wheeler@hotmail.com>
2022-02-21 14:57:27 -0800

    Mod SustListViewModel.java start adding observers

--- app/src/main/java/edu/cascadia/mobas/campusguidebook/viewmodel/SustListViewModel.java
+++ app/src/main/java/edu/cascadia/mobas/campusguidebook/viewmodel/SustListViewModel.java
@@ -13,16 +13,32 @@ import java.util.List;
 
 import edu.cascadia.mobas.campusguidebook.CampusGuidebookApp;
 
+import edu.cascadia.mobas.campusguidebook.data.model.Sustainability;
 import edu.cascadia.mobas.campusguidebook.data.repository.AppRepository;
 import edu.cascadia.mobas.campusguidebook.data.repository.ImageRepository;
-
+import edu.cascadia.mobas.campusguidebook.ui.Sustainability.SustUIItem;
 
 
 @RequiresApi(api = Build.VERSION_CODES.O)
 public class SustListViewModel extends AndroidViewModel {
 
+    private AppRepository mAppRepository = null;
+    private final MediatorLiveData<List<SustUIItem>> mLiveSustainabilityList = new MediatorLiveData<List<SustUIItem>>();
+    private ImageRepository mImageRepository = null;
 
+    public SustListViewModel(@NonNull Application application) {
+        super(application);
+        mAppRepository = ((CampusGuidebookApp) application).getAppRepository();
+        mImageRepository = ((CampusGuidebookApp) application).getImageRepository();
 
+        mLiveSustainabilityList.addSource(mAppRepository.getAllSustainability(), items -> {
+            List<SustUIItem> list = new ArrayList<SustUIItem>();
+            for (Sustainability sustainability : items) {
+                list.add(new SustUIItem(sustainability, mImageRepository.getImage(Sustainability.getImageUri())));
+            }
+            mLiveSustainabilityList.setValue(list);
+        });
+    }
 
 }
 
