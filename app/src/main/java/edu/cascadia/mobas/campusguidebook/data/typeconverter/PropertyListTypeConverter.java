7d90bf7d38c704d5dd787d3eee0f4f1445b82864
gomezfox <stephen@gomezfox.net>
2022-02-26 11:38:20 -0800

    Clubs update unfinished changes

---
+++ app/src/main/java/edu/cascadia/mobas/campusguidebook/data/typeconverter/PropertyListTypeConverter.java
@@ -0,0 +1,31 @@
+package edu.cascadia.mobas.campusguidebook.data.typeconverter;
+
+import androidx.annotation.Nullable;
+import androidx.room.TypeConverter;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.Map;
+
+public class PropertyListTypeConverter {
+    private static final Gson sGson = new Gson();
+
+    @TypeConverter
+    public static String toJson(@Nullable Map<String, String> properties) {
+        Type type = new TypeToken<Map<String, String>>(){}.getType();
+        return sGson.toJson(properties);
+    }
+
+    @TypeConverter
+    public static Map<String, String> toMap(@Nullable String json) {
+        return jsonStrToMap(json);
+    }
+
+    @TypeConverter
+    public static <T> Map<String, T> jsonStrToMap(String jsonStr) {
+        Type type = new TypeToken<Map<String, T>>() {}.getType();
+        return sGson.fromJson(jsonStr, type);
+    }
+}
