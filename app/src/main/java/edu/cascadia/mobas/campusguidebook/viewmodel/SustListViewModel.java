64593fa1d3bc61575f1ff141bcbc1df55924f7d5
Isaak Wheeler <isaak.wheeler@hotmail.com>
2022-02-26 17:16:49 -0800

    cleaned up code and fixed errors with permission scopes

--- app/src/main/java/edu/cascadia/mobas/campusguidebook/viewmodel/SustListViewModel.java
+++ app/src/main/java/edu/cascadia/mobas/campusguidebook/viewmodel/SustListViewModel.java
@@ -1,5 +1,7 @@
 package edu.cascadia.mobas.campusguidebook.viewmodel;
+
 import android.app.Application;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 
 import androidx.annotation.NonNull;
@@ -10,9 +12,7 @@ import androidx.lifecycle.MediatorLiveData;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import edu.cascadia.mobas.campusguidebook.CampusGuidebookApp;
-
 import edu.cascadia.mobas.campusguidebook.data.model.Sustainability;
 import edu.cascadia.mobas.campusguidebook.data.repository.AppRepository;
 import edu.cascadia.mobas.campusguidebook.data.repository.ImageRepository;
@@ -23,26 +23,27 @@ import edu.cascadia.mobas.campusguidebook.ui.Sustainability.SustUIItem;
 public class SustListViewModel extends AndroidViewModel {
 
     private AppRepository mAppRepository = null;
-    private final MediatorLiveData<List<SustUIItem>> mLiveSustainabilityList = new MediatorLiveData<List<SustUIItem>>();
+    private final MediatorLiveData<List<SustUIItem>> mLiveSustList = new MediatorLiveData<List<SustUIItem>>();
     private ImageRepository mImageRepository = null;
-
     public SustListViewModel(@NonNull Application application) {
         super(application);
+        // gets a reference to the repository
         mAppRepository = ((CampusGuidebookApp) application).getAppRepository();
         mImageRepository = ((CampusGuidebookApp) application).getImageRepository();
 
-        mLiveSustainabilityList.addSource(mAppRepository.getAllSustainability(), items -> {
+        // Observes the Sustainability list from the repository and emits a new one that also contains images
+        mLiveSustList.addSource(mAppRepository.getAllSustainability(), Sustainability -> {
             List<SustUIItem> list = new ArrayList<SustUIItem>();
-            for (Sustainability sustainability : items) {
+            for (Sustainability sustainability : Sustainability) {
                 list.add(new SustUIItem(sustainability, mImageRepository.getImage(sustainability.getImageUri())));
             }
-            mLiveSustainabilityList.setValue(list);
+            mLiveSustList.setValue(list);
         });
     }
 
+    // return a reference to the livedata containing all Sustianability and their images
     public LiveData<List<SustUIItem>> getAllSustainability() {
-        return mLiveSustainabilityList;
+        return mLiveSustList;
     }
-
 }
 
